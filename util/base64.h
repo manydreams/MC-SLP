a42e2c7069985b1588e9a491b4b52fc0f39669cf
damberva <2836145041@qq.com>
2024-10-13 03:21:44 +0800

    Added header protection to base64.h

--- util/base64.h
+++ util/base64.h
@@ -1,2 +1,7 @@
+#ifndef BASE64_H
+#define BASE64_H
+
 int base64_encode(char *input, const int input_len, char **output, int *output_len);
 int base64_decode(char *input, const int input_len, char **output, int *output_len);
+
+#endif /* BASE64_H */
